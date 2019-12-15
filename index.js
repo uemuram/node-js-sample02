1995ec8d07922899a7873dba888e6c2cf8c93373
uemuram <uemura.masaru1124@gmail.com>
2019-12-15 23:48:58 +0900

    expressインストール、作業準備

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var http = require("http");
+/*
+// Express利用しない版
 var port = process.env.PORT || 8000;
+var http = require("http");
 http.createServer(function (request, response) {
    response.writeHead(200, {"Content-Type": "text/plain"});
    response.end("Hello World7");
 }).listen(port);
+*/
+
