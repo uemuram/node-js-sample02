db207ba09b448aac42efa547b7eaa129bcc33c06
uemuram <uemura.masaru1124@gmail.com>
2019-12-16 23:16:29 +0900

    REST API

--- index.js
+++ index.js
@@ -8,3 +8,58 @@ http.createServer(function (request, response) {
 }).listen(port);
 */
 
+const express = require('express');
+const app = express();
+
+// 起動している箇所
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+app.use(express.json());
+
+// DBの代わり
+const courses = [
+   { id: 1, name: 'computer science'},
+   { id: 2, name: 'information technology'},
+   { id: 3, name: 'business intelligence'},
+];
+
+// GET /
+app.get('/', (req, res) => {
+   res.send('Simple REST API');
+});
+
+// GET /api/courses
+app.get('/api/courses', (req, res) => {
+   res.send(courses);
+});
+
+// POST /api/courses
+app.post('/api/courses', (req, res) => {
+   const course = {
+       id: courses.length + 1,
+       name: req.body.name
+   };
+   courses.push(course);
+   res.send(course);
+});
+
+// PUT /api/courses/1
+app.put('/api/courses/:id', (req, res) => {
+   const course = courses.find(c => c.id === parseInt(req.params.id));
+   if (!course) return res.status(404).send('The course with the given ID was not found.');
+
+   course.name = req.body.name;
+   res.send(course);
+});
+
+// DELETE /api/courses/1
+app.delete('/api/courses/:id', (req, res) => {
+   const course = courses.find(c => c.id === parseInt(req.params.id));
+   if (!course) return res.status(404).send('The course with the given ID was not found.');
+
+   const index = courses.indexOf(course);
+   courses.splice(index, 1);
+
+   res.send(course);
+});
